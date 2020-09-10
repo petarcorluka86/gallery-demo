521501e22184f06dafebb2790b86bc5191ca204e
petarcorluka86 <petarcorluka86@gmail.com>
2020-09-10 14:54:43 +0200

    title

--- src/Containers/App.js
+++ src/Containers/App.js
@@ -25,16 +25,19 @@ export default function App() {
   return (
     <div>
       <div className="app">
-        {urls.map( url => 
-          <div key={url} style={{border: "1pt solid black", borderRadius: "5pt", width: "150pt" , height: "150pt"}}>
-            <ProductImageSmall
-              image={{url:url, id: count++}}
-              openBigPicture={toggleModal}
-              h="100%"
-              w="150pt"
-            />
-          </div>
-        )}
+        <div className="title">Flom Products Gallery</div>
+        <div className="photos">
+          {urls.map( url => 
+            <div key={url} style={{border: "1pt solid black", borderRadius: "5pt", width: "150pt" , height: "150pt"}}>
+              <ProductImageSmall
+                image={{url:url, id: count++}}
+                openBigPicture={toggleModal}
+                h="100%"
+                w="150pt"
+              />
+            </div>
+          )}
+        </div>
       </div>
       <Modal show={showModal}>
         <BigPictureModal src={activeImage} urls={urls} closeModal={()=>setShowModal(!showModal)} openNewBig={(url,id) => setActiveImage({url:url,id:id})}/>
